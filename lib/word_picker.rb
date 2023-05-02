bf8b4bc503ef759e9d345c0c20288be2319fd737
Won Rhim <wwrk22@gmail.com>
2023-05-02 14:45:27 -0400

    Refactor

--- lib/word_picker.rb
+++ lib/word_picker.rb
@@ -4,24 +4,26 @@ class WordPicker
     @filename = filename
   end
 
+  # Pick a word at random from the current file.
   def random_word
     file = File::open(@filename, 'r')
-
-    # Get number of lines
-    output = `wc -l ./words.txt`.strip!
-    num_lines = output.split[0].to_i if output
-
-    # Pick a random line pos
-    rand_line_num = Random::rand(num_lines + 1)
-
-    # Go to that line and get the word
-    (rand_line_num - 1).times { file.readline }
+    (random_line_num - 1).times { file.readline }
     word = file.readline.chomp
-    puts word
+    file.close # Don't let the fd live for the life of the program
+    return word
+  end
+
+
+  private
+
+  def random_line_num
+    output = `wc -l ./words.txt`.strip
+    num_lines = output.split[0].to_i if output
+    return Random::rand(num_lines + 1) 
   end
 
 end
 
 
 wp = WordPicker.new('./words.txt')
-wp.random_word
+puts wp.random_word
