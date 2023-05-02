d47ede7c1a3dbde9e74faf9128ca3c1075198269
Won Rhim <wwrk22@gmail.com>
2023-05-02 15:25:07 -0400

    Add guess with validation

--- lib/game.rb
+++ lib/game.rb
@@ -16,9 +16,28 @@ class Game
     @word.each_char { |ch| @guess_pool.include?(ch) ? (print ch) : (print '_') }
     puts
   end
+
+  def guess
+    letter = gets.chomp
+    letter_added = add_to_pool(letter) if alphabet?(letter)
+  end
+
+  def alphabet?(letter)
+    return false if letter.length != 1
+    ord = letter.ord
+    return (65 <= ord && ord <= 90) || (97 <= ord && ord <= 122)
+  end
+
+  def add_to_pool(letter)
+    return false if @guess_pool.include? letter
+    @guess_pool << letter
+    return true
+  end
 end
 
 
 g = Game.new('./words.txt')
 g.start
 g.display_progress
+g.guess
+g.guess
